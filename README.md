cee7ad07197f5e40b958ad9c5c94dc1f5fb1e0d7
ovyvo <vue12306@163.com>
2023-12-14 10:06:39 +0800

    :memo: docs: docs

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 - 远程控制（Electron进程间通信,WebRTC,websocket信令服务等知识）
 #### 资源
 vite + vue3 + electron
+学习参考：极客时间邓耀龙Electron项目实战
+#### 未完成
+- robotjs集成及对应的信令服务
+- 项目打包流程及更新
+- 集成原生c++
 #### 注意
 - 本地调试需要使用localhost地址，防止浏览器保护机制限制navigator对象访问不到全部属性
+- 部分未完成任务需要配合本地开发环境安装Python之类的依赖，具体可自行百度
--- electron/main/createAboutWindow.js
+++ electron/main/createAboutWindow.js
@@ -2,10 +2,10 @@ const openAboutWindow = require('about-window').default
 const path = require('path')
 
 const create = () => openAboutWindow({
-  icon_path: path.join(__dirname, 'icon.png'),
-  package_json_dir: path.resolve(__dirname, '/../../'),
-  cropyright: 'Copyright (c) 2023 yan',
-  homepage: 'https://github.com/',
+  icon_path: path.join(__dirname, './trayAndMenu/icon.png'),
+  package_json_dir: path.resolve(__dirname, '../../'),
+  cropyright: 'Copyright (c) 2023 RyanYan',
+  homepage: 'https://github.com/OVYVO/electron-case',
 })
 
 module.exports = { create }
